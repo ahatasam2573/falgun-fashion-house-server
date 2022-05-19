9519371ef5776fbf3081da9a807134b8d3d97906
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-19 21:59:11 +0600

    heroku created

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(express.json());
 
 
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lalri.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
